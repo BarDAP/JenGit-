ba7a5abe245cc0b3920a64652963fe4ebc7ea177
BarDAP <barbenshabat@gmail.comgit config --global user.name BarDAP>
2021-11-07 21:18:43 +0200

    crete another fill, saw no output on the consol, made the last change on hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello{
 	public static void main(String[] args){
-		System.out.println("Change number 2");
+		System.out.println("after created another java file I understood that the consul output will show only what's under execute shell.");
 	}
 }

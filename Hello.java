5172f4064017e9f0e60aec4cc1030c95ab2023d9
BarDAP <barbenshabat@gmail.comgit config --global user.name BarDAP>
2021-11-07 21:07:51 +0200

    fixed the public and made text to change

--- Hello.java
+++ Hello.java
@@ -1,8 +1,5 @@
-ublic class Hello{
+public class Hello{
 	public static void main(String[] args){
-		for (int i=1; i<=10; i++){
-			System.out.println("Hello world..."+i);
-
-		}
+		System.out.println("Change number 2");
 	}
 }
